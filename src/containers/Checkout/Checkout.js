7a3dae2fa286c02d1341b7f1f62e704300c7c5ad
Olga Bazhenova <olgabazhenova07@yandex.ru>
2020-09-13 21:32:08 +0300

    Add Redux to the project

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,29 +1,11 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
-  initState = () => {
-    const queryInit = new URLSearchParams(
-      this.props.location.search
-    );
-    const ingredientsInit = {};
-    let priceInit = 0;
-    for (let param of queryInit.entries()) {
-      // ['salad', '1']
-      if (param[0] === 'price') {
-        priceInit = param[1];
-      } else {
-        ingredientsInit[param[0]] = +param[1];
-      }
-    }
-    return {
-      ingredients: ingredientsInit,
-      totalPrice: priceInit
-    };
-  };
-  state = this.initState();
 
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
@@ -37,15 +19,21 @@ class Checkout extends Component {
     return (
       <div>
         <CheckoutSummary
-          ingredients={this.state.ingredients}
+          ingredients={this.props.ings}
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler} />
         <Route
           path={this.props.match.path + '/contact-data'}
-          render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />)} />
+          component={ContactData} />
       </div>
     );
   }
 }
 
-export default Checkout;
+const mapStateToProps = (state) => {
+  return {
+    ings: state.ingredients
+  };
+};
+
+export default connect(mapStateToProps)(Checkout);
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -12,7 +12,7 @@ class Orders extends Component {
   componentDidMount () {
     axios.get('/orders.json')
       .then(res => {
-        const fetchedOrders = []
+        const fetchedOrders = [];
         for (let key in res.data) {
           fetchedOrders.push({
             ...res.data[key],
