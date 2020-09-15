e56500984ee1409f2637ca4d06e623f302192e0a
Olga Bazhenova <olgabazhenova07@yandex.ru>
2020-09-15 21:09:21 +0300

    Fetch ingredients asynchronously

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -13,24 +13,11 @@ import * as burgerBuilderActions from '../../store/actions/index';
 
 class BurgerBuilder extends Component {
   state = {
-    purchasing: false,
-    loading: false,
-    error: false
+    purchasing: false
   }
 
   componentDidMount () {
     console.log(this.props);
-    // axios.get('https://react-my-burger-20b27.firebaseio.com/ingredients.json')
-    // .then(response => {
-    //   this.setState({
-    //     ingredients: response.data
-    //   });
-    // })
-    // .catch(error => {
-    //   this.setState({
-    //     error: true
-    //   });
-    // });
   }
 
   updatePurchaseState (ingredients) {
@@ -89,10 +76,6 @@ class BurgerBuilder extends Component {
         price={this.props.price}
         purchaseCancelled={this.purchaseCancelHandler}
         purchaseContinued={this.purchaseContinueHandler} />;
-    }
-
-    if (this.state.loading) {
-      orderSummary = <Spinner />;
     }
 
     return (
